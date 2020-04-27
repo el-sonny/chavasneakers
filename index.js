4090edc15112b83a5b1dde3bd3636901611ecf45
El Sonny <sonny@spaceshiplabs.com>
2020-04-26 20:40:34 -0500

    mvp ;)

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ async function loadJSON(file) {
     return JSON.parse(data);
 };
 
+async function saveJSON(filename, json) {
+    return fs.writeFile(filename, JSON.stringify(json));
+};
+
 async function execute() {
-    await loadJSON();
+    const data = await loadJSON('./data/nike-waffle-racer-offwite-black-sales.json');
+    const sales = data.ProductActivity;
+
+    let sizes = {};
+    sales.forEach(sale => {
+        if (!sizes[sale.shoeSize]) {
+            sizes[sale.shoeSize] = {};
+        }
+        let date = new Date(sale.createdAt);
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        let compositeKey = month.toString() + '-' + year.toString();
+        if (!sizes[sale.shoeSize][compositeKey]) {
+            sizes[sale.shoeSize][compositeKey] = 0;
+        }
+        sizes[sale.shoeSize][compositeKey] += 1;
+    });
+    const res = await saveJSON('./output/nike-waffle-racer-offwite-black-sales-summary.json',sizes);
 };
 
 execute();
