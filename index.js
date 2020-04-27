51a06d50a966e869f7c67c27e625468e4f0a7523
El Sonny <sonny@spaceshiplabs.com>
2020-04-26 21:43:28 -0500

    add price totals

--- index.js
+++ index.js
@@ -13,21 +13,26 @@ async function execute() {
     const data = await loadJSON('./data/nike-waffle-racer-offwite-black-sales.json');
     const sales = data.ProductActivity;
 
-    let sizes = {};
+    let counts = {};
+    let income = {};
     sales.forEach(sale => {
-        if (!sizes[sale.shoeSize]) {
-            sizes[sale.shoeSize] = {};
+        if (!counts[sale.shoeSize]) {
+            counts[sale.shoeSize] = {};
+            income[sale.shoeSize] = {};
         }
         let date = new Date(sale.createdAt);
         let month = date.getMonth() + 1;
         let year = date.getFullYear();
         let compositeKey = month.toString() + '-' + year.toString();
-        if (!sizes[sale.shoeSize][compositeKey]) {
-            sizes[sale.shoeSize][compositeKey] = 0;
+        if (!counts[sale.shoeSize][compositeKey]) {
+            counts[sale.shoeSize][compositeKey] = 0;
+            income[sale.shoeSize][compositeKey] = 0;
         }
-        sizes[sale.shoeSize][compositeKey] += 1;
+        counts[sale.shoeSize][compositeKey] += 1;
+        income[sale.shoeSize][compositeKey] += sale.amount;
     });
-    const res = await saveJSON('./output/nike-waffle-racer-offwite-black-sales-summary.json',sizes);
+    const res = await saveJSON('./output/nike-waffle-racer-offwite-black-sales-summary.json', counts);
+    const res2 = await saveJSON('./output/nike-waffle-racer-offwite-black-sales-income-summary.json', income);
 };
 
 execute();
